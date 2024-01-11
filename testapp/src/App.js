41c9b8052d8a54564b6dc9c1b97470a7206f16da
Mudipa kishan imayanga <128371632+mk1shan@users.noreply.github.com>
2024-01-11 23:13:27 +0530

    img

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import reactLogo from './thumbbig-1328014.jpg';
 
 function App() {
   const navigate = useNavigate();
@@ -8,7 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Mudipa's page</h1>
-        <button className='users-button' onClick={() => navigate('users')}>Users</button>
+        <img src={reactLogo} alt="thumbbig-1328014.jpg" />
+        <button className="users-button" onClick={() => navigate('users')}>
+          Users
+        </button>
       </header>
     </div>
   );
