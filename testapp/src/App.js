701be2b4236693a685abf0752d32dd3f40771246
Mudipa kishan imayanga <128371632+mk1shan@users.noreply.github.com>
2024-01-17 00:19:04 +0530

    next

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import './App.css';
-import reactLogo from './thumbbig-1328014.jpg';
+import reactLogo from './robert-pattinson-the-batman-2021-movies-artwork-batman-hd-wallpaper-a9a0f86d415a9d7b46b7780f3001163d.jpg';
 
 function App() {
   const navigate = useNavigate();
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Mudipa's page</h1>
-        <img src={reactLogo} alt="thumbbig-1328014.jpg" />
+        <img src={reactLogo} alt="robert-pattinson-the-batman-2021-movies-artwork-batman-hd-wallpaper-a9a0f86d415a9d7b46b7780f3001163d.jpg" />
         <button className="users-button" onClick={() => navigate('users')}>
           Users
         </button>
